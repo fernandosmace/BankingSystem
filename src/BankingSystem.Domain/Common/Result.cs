e92205b6c5d32b9d8d60b02fc9a6a479b0056168
Fernando Soares Macedo <fernando.soares.macedo@hotmail.com>
2025-03-23 18:44:03 -0300

    feat: Add Result and Account class files.

---
+++ src/BankingSystem.Domain/Common/Result.cs
@@ -0,0 +1,48 @@
+﻿using Flunt.Notifications;
+
+namespace BankingSystem.Domain.Common;
+
+public class Result
+{
+    public bool Success { get; private set; }
+    public IReadOnlyCollection<Notification> Notifications { get; private set; }
+    public string Message { get; private set; }
+
+    protected Result(bool success, IReadOnlyCollection<Notification> notifications, string message)
+    {
+        Success = success;
+        Notifications = notifications ?? new List<Notification>();
+        Message = message;
+    }
+
+    public static Result Ok(string message = "")
+    {
+        return new Result(true, new List<Notification>(), message);
+    }
+
+    public static Result Fail(IReadOnlyCollection<Notification> notifications, string message = "")
+    {
+        return new Result(false, notifications, message);
+    }
+}
+
+public class Result<T> : Result
+{
+    public T Data { get; private set; }
+
+    private Result(bool success, IReadOnlyCollection<Notification> notifications, T data, string message)
+        : base(success, notifications, message)
+    {
+        Data = data;
+    }
+
+    public static Result<T> Ok(T data, string message = "")
+    {
+        return new Result<T>(true, new List<Notification>(), data, message);
+    }
+
+    public static Result<T> Fail(IReadOnlyCollection<Notification> notifications, T data = default, string message = "")
+    {
+        return new Result<T>(false, notifications, data, message);
+    }
+}
---
+++ src/BankingSystem.Domain/Entities/Account.cs
@@ -0,0 +1,63 @@
+﻿using BankingSystem.Domain.Common;
+using Flunt.Notifications;
+using Flunt.Validations;
+
+namespace BankingSystem.Domain.Entities;
+public class Account : Notifiable<Notification>
+{
+    public Guid Id { get; private set; }
+    public string Name { get; private set; }
+    public string Document { get; private set; }
+    public decimal Balance { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+    public bool IsActive { get; private set; }
+
+    private Account() { }
+
+    public Account(string name, string document)
+    {
+        Id = Guid.NewGuid();
+        Name = name;
+        Document = document;
+        Balance = 1000m;
+        CreatedAt = DateTime.UtcNow;
+        IsActive = true;
+
+        Validate();
+    }
+
+    public void Validate()
+    {
+        AddNotifications(new Contract<Notification>()
+            .Requires()
+            .IsNotNullOrWhiteSpace(Name, "Account.Name", "O nome não pode ser vazio")
+            .IsNotNullOrWhiteSpace(Document, "Account.Document", "O documento não pode ser vazio")
+        );
+    }
+
+    public void Deactivate() => IsActive = false;
+
+    public void UpdateBalance(decimal amount)
+    {
+        if (amount != 0)
+            Balance += amount;
+    }
+
+    public Result Transfer(decimal amount, Account destinationAccount)
+    {
+        var transferContract = new Contract<Notification>()
+                .Requires()
+                .IsTrue(IsActive, "Account.IsActive", "A conta de origem precisa estar ativa.")
+                .IsGreaterOrEqualsThan(Balance, amount, "Account.Balance", "A conta ded origem não possui saldo suficiente para a transferência.")
+                .IsTrue(destinationAccount.IsActive, "Account.IsActive", "A conta de destino precisa estar ativa.");
+
+        if (transferContract.IsValid)
+        {
+            UpdateBalance(-amount);
+            destinationAccount.UpdateBalance(amount);
+            return Result.Ok("Transferência realizada com sucesso!");
+        }
+
+        return Result.Fail(transferContract.Notifications, "Falha na realização da transferência!");
+    }
+}
